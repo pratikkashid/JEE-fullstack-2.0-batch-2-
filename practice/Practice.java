d13a2736715155e39aeb02450175131c0c10909b
pratikkashid <pratikkashid11@gmail.com>
2021-06-09 08:46:42 +0530

    new update
    Signed-off-by: pratikkashid <pratikkashid11@gmail.com>

--- practice/Practice.java
+++ practice/Practice.java
@@ -4,6 +4,6 @@ public class Practice {
 	public static void main(String[] args) {
 		System.out.println("hello java");
 		System.out.println("hgdfjhgfjh");
-		//System.out.println("");
+		System.out.println("");
 	}
 }
